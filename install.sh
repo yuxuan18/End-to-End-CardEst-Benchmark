5c8eb905bd4c7f3fe2cb2f34e2b11e2c68768b47
Yuxuan Zhu <yxx404@illinois.edu>
2024-08-06 11:17:49 -0400

    pipeline

---
+++ install.sh
@@ -0,0 +1,57 @@
+# setup options
+if [ -z "$DB_PATH" ]; then
+    DB_PATH="."
+fi
+IMDB_DATASETS_DIRECTORY=$DB_PATH/imdb_data
+
+if [-z "$BENCHMARK"]; then
+    BENCHMARK="JOB"
+fi
+
+# install anaconda
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+. ~/.bashrc
+
+# setup conda env
+conda create -y -n pilotdb
+conda activate pilotdb
+conda install -y -c conda-forge postgresql
+conda install -y python=3.11
+
+# setup database
+if $BENCHMARK == "JOB"; then
+    initdb -D $DB_PATH/postgres_db
+    pg_ctl -D $DB_PATH/postgres_db -l $DB_PATH/postgres_db/logfile start
+    wget http://homepages.cwi.nl/~boncz/job/imdb.tgz
+    mkdir $DB_PATH/imdb_data
+    tar -xvzf imdb.tgz -C $DB_PATH/imdb_data
+    psql -d postgres -f $DB_PATH/imdb_data/schematext.sql
+    cat scripts/sql/imdb_create.sql | sed "s|\$IMDB_DATASETS_DIRECTORY|$IMDB_DATASETS_DIRECTORY|g" | psql -d postgres
+    psql -d postgres -f scripts/sql/imdb_index.sql
+fi
+
+if $BENCHMARK == "STATS"; then
+    initdb -D $DB_PATH/postgres_db
+    pg_ctl -D $DB_PATH/postgres_db -l $DB_PATH/postgres_db/logfile start
+    psql -d postgres -f datasets/stats_simplified/stats.sql
+    psql -d postgres -f scripts/sql/stats_load.sql
+    psql -d postgres -f scripts/sql/stats_index.sql
+fi
+
+if $BENCHMARK == "BOTH"; then
+    initdb -D $DB_PATH/postgres_db
+    pg_ctl -D $DB_PATH/postgres_db -l $DB_PATH/postgres_db/logfile start
+    wget http://homepages.cwi.nl/~boncz/job/imdb.tgz
+    mkdir $DB_PATH/imdb_data
+    tar -xvzf imdb.tgz -C $DB_PATH/imdb_data
+    psql -d postgres -f $DB_PATH/imdb_data/schematext.sql
+    cat scripts/sql/imdb_create.sql | sed "s|\$IMDB_DATASETS_DIRECTORY|$IMDB_DATASETS_DIRECTORY|g" | psql -d postgres
+    psql -d postgres -f scripts/sql/imdb_index.sql
+    psql -d postgres -f datasets/stats_simplified/stats.sql
+    psql -d postgres -f scripts/sql/stats_load.sql
+    psql -d postgres -f scripts/sql/stats_index.sql
+fi
---
+++ run_queries.py
@@ -0,0 +1,38 @@
+import os
+import json
+import subprocess
+
+def extract_time(time_str):
+    start_pos = time_str.find("Time: ") + len("Time: ")
+    end_pos = time_str.find(" ms")
+    return float(time_str[start_pos:end_pos])
+
+with open("workloads/job-light/job_light_queries.sql") as f:
+    qid = 1
+    for line in f:
+        results = []
+        for i in range(6):
+            query = "EXPLAIN ANALYZE " + line
+            command = "psql -d postgres -c \"" + query + "\""
+            # execute the command and fetch the output
+            output = subprocess.check_output(command, shell=True)
+            if i == 0:
+                continue
+            # convert to string
+            output = output.decode("utf-8")
+            output = output.strip().split("\n")
+            plan = output[:-4]
+            exec_time_str = output[-2]
+            plan_time_str = output[-3]
+            exec_time = extract_time(exec_time_str)
+            plan_time = extract_time(plan_time_str)
+            results.append({
+                "plan_time": plan_time,
+                "exec_time": exec_time,
+                "plan": plan,
+            })
+        # write the results to a file
+        with open(f"results/{qid}.jsonl", "a+") as f:
+            for result in results:
+                f.write(json.dumps(result) + "\n")
+        qid += 1
